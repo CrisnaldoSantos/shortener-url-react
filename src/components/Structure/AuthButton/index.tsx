28193e1e4990384b0983b517b5fb714d32a029e6
CrisnaldoSantos <crisnaldo.santos@dcomp.ufs.br>
2022-02-20 21:37:31 -0300

    feat: include dynamic email in navbar

--- src/components/Structure/AuthButton/index.tsx
+++ src/components/Structure/AuthButton/index.tsx
@@ -3,17 +3,18 @@ import { useNavigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { NavDropdown, Form, Button } from 'react-bootstrap';
 import { logout } from 'store/auth/auth.ducks';
+import { getUserData } from 'utils/dataStorage';
 
 export function AuthButton() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const { loginSuccess } = useSelector((state: RootState) => state.auth);
-
+  const user = getUserData();
   return (
     <>
       {loginSuccess ? (
         <NavDropdown
-          title="crisnaldo72@gmail.com"
+          title={user.email}
           id="auth-nav-dropdown"
           className="text-secondary"
         >
